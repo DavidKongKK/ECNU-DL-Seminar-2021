f205003553ce2855a0db99a43a0d1c504c18c516
DavidKongKK <davidkong0725@outlook.com>
2021-07-06 21:00:19 +0800

    添加注释

--- Week 2/0706正则化作业/regularization.py
+++ Week 2/0706正则化作业/regularization.py
@@ -104,9 +104,8 @@ def compute_cost_with_regularization(A3, Y, parameters, lambd):
     cross_entropy_cost = compute_cost(A3, Y)
 
     # START CODE HERE ### (approx. 1 line)
-    L2_regularization_cost = lambd / \
-        (2 * m) * (np.sum(np.square(W1)) +
-                   np.sum(np.square(W2)) + np.sum(np.square(W3)))
+    #除以2的原因是在求导的时候抵消掉
+    L2_regularization_cost = lambd / (2 * m) * (np.sum(np.square(W1)) +np.sum(np.square(W2)) + np.sum(np.square(W3)))
     ### END CODER HERE ###
 
     cost = cross_entropy_cost + L2_regularization_cost
@@ -136,7 +135,7 @@ def backward_propagation_with_regularization(X, Y, cache, lambd):
     dZ3 = A3 - Y
 
     # START CODE HERE ### (approx. 1 line)
-    dW3 = 1./m*(np.dot(dZ3, A2.T)+lambd*W3)
+    dW3 = 1./m * (np.dot(dZ3, A2.T)+lambd*W3)
     ### END CODE HERE ###
     db3 = 1. / m * np.sum(dZ3, axis=1, keepdims=True)
 
@@ -148,12 +147,12 @@ def backward_propagation_with_regularization(X, Y, cache, lambd):
     db2 = 1. / m * np.sum(dZ2, axis=1, keepdims=True)
 
     dA1 = np.dot(W2.T, dZ2)
-    dZ1 = np.multiply(dA1, np.int64(A1 > 0))
+    dZ1 = np.multiply(dA1, np.int64(A1 > 0)) #TODO:什么意思？
     # START CODE HERE ### (approx. 1 line)
     dW1 = 1./m*(np.dot(dZ1,X.T)+lambd*W1)
     ### END CODE HERE ###
     db1 = 1. / m * np.sum(dZ1, axis=1, keepdims=True)
-
+    
     gradients = {"dZ3": dZ3, "dW3": dW3, "db3": db3, "dA2": dA2,
                  "dZ2": dZ2, "dW2": dW2, "db2": db2, "dA1": dA1,
                  "dZ1": dZ1, "dW1": dW1, "db1": db1}
